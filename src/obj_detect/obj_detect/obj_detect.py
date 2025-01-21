6ec0549cf453196d3ea7eee3f1d00c64f193a791
Team-B <Team-B@imi-pr081>
2025-01-21 18:20:08 +0100

    extract boxes including params from result

--- src/obj_detect/obj_detect/obj_detect.py
+++ src/obj_detect/obj_detect/obj_detect.py
@@ -13,10 +13,36 @@ class ImageProcessor(Node):
         self.yolo_model = YOLO("src/obj_detect/weights/best.pt")
         self.cvbridge = CvBridge()
 
+        self.label_lookup = {
+            0: "arrow_left",
+            1: "arrow_right",
+            2: "patch",
+            3: "lava",
+            4: "path"
+        }
+
     def listener_callback(self, msg):
         image = self.cvbridge.imgmsg_to_cv2(img_msg=msg, desired_encoding="bgr8")
         result = self.yolo_model.predict(image)[0].cpu()
+
+        result_prob = [box.conf.item() for box in result.boxes]
+        #print("Probabilities:", result_prob)
+
+        result_coords = [box.xyxy for box in result.boxes]
+        #print("Bounding Box Coordinates:", result_coords)
+
+        result_labels = [box.cls.item() for box in result.boxes]
+        #print("Class Indices (Labels):", result_labels)
+
+        #print(result_prob)
         self.publisher_.publish(self.cvbridge.cv2_to_imgmsg(result.plot()))
+
+        boxes = [{
+            "label": self.label_lookup[int(box.cls.item())],
+            "conf": box.conf.item(),
+            "box": box.xyxy.tolist()[0]
+        } for box in result.boxes]
+        print(boxes)
 
 
 
