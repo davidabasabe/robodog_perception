b50e372a0c431ee690a868c1cbc285c4ec5534fa
Team-B <Team-B@imi-pr081>
2025-01-28 17:27:03 +0100

    publishers for lava and arrows

--- src/obj_detect/obj_detect/obj_detect.py
+++ src/obj_detect/obj_detect/obj_detect.py
@@ -15,6 +15,8 @@ class ImageProcessor(Node):
         self.depth_subscriber = self.create_subscription(Image, '/rs/Image/Depth', self.depth_callback, 10)
         self.subscriber_rs_intrinsics = self.create_subscription(Intrinsics, '/rs/Intrinsics', self.rs_intrinsics_callback, 1)
         self.lava_pub = self.create_publisher(YoloDetection, '/lava_detection', 10)
+        self.arrow_left_pub = self.create_publisher(YoloDetection, '/arrow_left', 10)
+        self.arrow_right_pub = self.create_publisher(YoloDetection, '/arrow_right', 10)
         self.yolo_model = YOLO("src/obj_detect/weights/best.pt")
         self.cvbridge = CvBridge()
         self.depth_intrinsics = rs.pyrealsense2.intrinsics()
@@ -46,19 +48,52 @@ class ImageProcessor(Node):
             "coord": get_midpoint(box.xyxy.tolist()[0])
         } for box in result.boxes]
 
-        #for box in boxes:
-        #    if box["label"] != "path": return
-        #    img_x, img_y = box["coord"]
-        #    distance = self.depth_array[img_y, img_x]*self.depth_scale
-        #    coords = self.pixel_to_point(int(img_x), int(img_y))
-        #    lava_msg = YoloDetection()
-        #    lava_msg.x, 
+        def get_coords(box):
+            img_x, img_y = box["coord"]
+            distance = self.depth_array[img_y, img_x]*self.depth_scale
+            coords = self.pixel_to_point(int(img_x), int(img_y))
+            return coords, distance
+        
+        classes = [e["label"] for e in boxes]
 
+        lava_msg = YoloDetection()
+        lava_msg.class_name = "lava"
+        if "path" in classes:
+            box = boxes[classes.index("path")]
+            coords, dist = get_coords(box["coord"])
+            lava_msg.x, lava_msg.y, lava_msg.z = coords
+            lava_msg.distance = dist
+            lava_msg.detected = True
+        else:
+            lava_msg.detected = False
 
+        al_msg = YoloDetection()
+        al_msg.class_name = "arrow_left"
+        if "arrow_left" in classes:
+            box = boxes[classes.index("arrow_left")]
+            coords, dist = get_coords(box["coord"])
+            al_msg.x, al_msg.y, al_msg.z = coords
+            al_msg.distance = dist
+            al_msg.detected = True
+        else:
+            al_msg.detected = False
 
+        ar_msg = YoloDetection()
+        ar_msg.class_name = "arrow_right"
+        if "arrow_right" in classes:
+            box = boxes[classes.index("arrow_right")]
+            coords, dist = get_coords(box["coord"])
+            ar_msg.x, ar_msg.y, ar_msg.z = coords
+            ar_msg.distance = dist
+            ar_msg.detected = True
+        else:
+            ar_msg.detected = False
 
-        #return
-        # Testing with paths class only
+        self.lava_pub.publish(lava_msg)
+        self.arrow_left_pub.publish(al_msg)
+        self.arrow_right_pub.publish(ar_msg)
+
+        return      # Testing with paths class only
 
         labels = [e["label"] for e in boxes]
         if "path" in labels:
@@ -85,12 +120,12 @@ class ImageProcessor(Node):
         self.depth_intrinsics.coeffs = msg.coeffs
 
     def pixel_to_point(self, x, y):
-        coordinate = rs.rs2_deproject_pixel_to_point(
+        point = rs.rs2_deproject_pixel_to_point(
             self.depth_intrinsics,
             [y, x],
             self.depth_array[y, x]*self.depth_scale
             )
-        return coordinate
+        return [point[1], point[0], point[2]]   # rs2 is kinda fucked
 
 def main(args=None):
     rclpy.init(args=args)
