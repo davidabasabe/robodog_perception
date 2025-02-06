f318b80b7e7202d9ebc0a82057e334024606ffb8
davidabasabe <dabasabe@hotmail.es>
2025-02-07 00:11:21 +0100

    Best step detection ever

--- src/step_detection/step_detection/step_detection.py
+++ src/step_detection/step_detection/step_detection.py
@@ -4,10 +4,11 @@ from sensor_msgs.msg import PointCloud2
 import numpy as np
 import open3d as o3d
 import sensor_msgs_py.point_cloud2 as pc2
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, PointField
 from collections import defaultdict
 from numpy.lib import recfunctions as rfn
 from robot_interfaces.msg import Stair
+from std_msgs.msg import Header
 
 import matplotlib.pyplot as plt
 import random
@@ -19,40 +20,70 @@ class StepDetection(Node):
         super().__init__('step_detection')
         self.subscriber_ = self.create_subscription(PointCloud2, '/go2/Lidar', self.listener_callback, 10)
         self.publisher_ = self.create_publisher(Stair, '/stair_detection', 10)
+        self.publisher_vertical_planes = self.create_publisher(PointCloud2, '/vertical_planes', 10)
+        self.publisher_horizontal_planes = self.create_publisher(PointCloud2, '/horizontal_planes', 10)
         self.current_lidar_points = []
         self.moving_average_height_diff = deque()
         self.moving_average_ground_height = deque()
         self.stair_msg = Stair()
-
-    def listener_callback(self, msg):
-        pcd = self.msg_to_pcd(msg)
-        if type(pcd) is None: return
-
-        self.current_lidar_points.append(np.asarray(pcd.points))
-
-        if len(self.current_lidar_points) < 2:
-            print("Not enough Lidar points")
-            return
-
-        if len(self.current_lidar_points) > 2:
-            self.current_lidar_points.pop(0)
-
-        compound_points = np.vstack(self.current_lidar_points)
-        #compound_points = self.crop_point_cloud(compound_points, x_range=(0.4, 1.5), y_range=(-0.4, 0.4), z_range=(-3.0, -0.15))
-        compound_points = self.crop_point_cloud(compound_points, x_range=(0.4, 1.5), y_range=(-0.5, 0.5), z_range=(-1.0, -0.15))
-        compound_cloud = o3d.geometry.PointCloud()
-        compound_cloud.points = o3d.utility.Vector3dVector(compound_points)
-
-        self.find_planes(compound_cloud)
-        return
-
+        self.ground_height = 0
 
     def msg_to_pcd(self, msg):
         point_cloud = pc2.read_points(msg)
         point_cloud = rfn.structured_to_unstructured(point_cloud[['x', 'y', 'z']])
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(point_cloud)
-        return pcd        
+        return pcd
+
+    def listener_callback(self, msg):
+
+        pcd = self.msg_to_pcd(msg)
+        if type(pcd) is None: return
+
+        distance_threshold = 0.05
+        ransac_n = 5
+        num_iterations = 200
+        copy_point_cloud = pcd
+        plane_model, inliers = copy_point_cloud.segment_plane(
+                distance_threshold=distance_threshold,
+                ransac_n=ransac_n,
+                num_iterations=num_iterations
+            )
+        
+        ground_plane = copy_point_cloud.select_by_index(inliers)
+        new_ground_height = np.mean(np.asarray(ground_plane.points)[:, 2])
+        self.ground_height = new_ground_height
+        
+        copy_point_cloud = copy_point_cloud.select_by_index(inliers, invert=True)
+        cropped_points = np.asarray(copy_point_cloud.points)
+        cropped_points = self.crop_point_cloud(cropped_points, x_range=(0.3, 1), y_range=(-0.3, 0.3), z_range=(-1.0, 0.7))
+        cropped_cloud = o3d.geometry.PointCloud()
+        cropped_cloud.points = o3d.utility.Vector3dVector(cropped_points)
+        z_points_obstacles = np.asarray(cropped_cloud.points)[:, 2]
+        x_points_obstacles = np.asarray(cropped_cloud.points)[:, 0]
+        height_diff = None
+        closest_x_point = 10.0
+        if z_points_obstacles.size > 0:
+            highest_obstacle_point = np.max(z_points_obstacles)
+            lowest_obstacle_point = np.min(z_points_obstacles)
+            height_diff = self.calculate_step_height(highest_obstacle_point, lowest_obstacle_point, new_ground_height)
+            closest_x_point = self.resolve_closest_x_point(height_diff, z_points_obstacles, x_points_obstacles)
+            #print(f"Obstacle height difference is: {height_diff}")
+            #print(f"Obstacle distance is: {closest_x_point}")
+        self.resolve_step(height_diff, closest_x_point)
+        return
+    
+    def calculate_step_height(self, highest: float, lowest: float, ground: float) -> float:
+
+        obstacle_height = self.choose_height_plane(highest, lowest, ground)
+        height_diff = obstacle_height - ground
+        return height_diff
+    
+    def choose_height_plane(self, highest: float, lowest: float, ground: float) -> float:
+        mean_height = (highest + lowest) / 2
+        if mean_height < ground:
+            return lowest
+        return highest        
     
     def crop_point_cloud(self, point_cloud, x_range, y_range, z_range):
         """Crop the point cloud to a region of interest."""
@@ -61,106 +92,66 @@ class StepDetection(Node):
                 (point_cloud[:, 2] > z_range[0]) & (point_cloud[:, 2] < z_range[1])
         return point_cloud[mask]
     
-    def find_planes(self, point_cloud):
-        distance_threshold = 0.04
-        ransac_n = 10
-        num_iterations = 50
-        vertical_z_points = []
-        vertical_x_points = []
-        plane_z_points = []
-        plane_x_points = []
-        copy_point_cloud = point_cloud
-        min_points_for_plane = 10
-
-        plane_model, inliers = copy_point_cloud.segment_plane(
-                distance_threshold=distance_threshold,
-                ransac_n=ransac_n,
-                num_iterations=num_iterations
-            )
-        copy_point_cloud = copy_point_cloud.select_by_index(inliers, invert=True)
-
-        while len(copy_point_cloud.points) > min_points_for_plane:
-            plane_model, inliers = copy_point_cloud.segment_plane(
-                distance_threshold=distance_threshold,
-                ransac_n=ransac_n,
-                num_iterations=num_iterations
-            )
-            if len(inliers) < min_points_for_plane:
-                break         
-            normal_vector = plane_model[:3]
-
-            # Check if the plane is horizontal (normal vector close to Z-axis)
-            if np.abs(normal_vector[2]) > 0.90:
-                inlier_cloud = point_cloud.select_by_index(inliers)
-                labels = np.array(inlier_cloud.cluster_dbscan(eps=0.02, min_points=2, print_progress=False))
-                valid_labels = labels[labels != -1]
-
-                if valid_labels.size < 1:
-                    copy_point_cloud = copy_point_cloud.select_by_index(inliers, invert=True)
-                    continue
-                # Find the largest valid cluster of points
-                unique_labels, counts = np.unique(labels, return_counts=True)
-                largest_cluster_label = unique_labels[np.argmax(counts)]
-                largest_cluster_indices = np.where(labels == largest_cluster_label)[0]
-                inlier_cloud = inlier_cloud.select_by_index(largest_cluster_indices)
-                plane_z_points.append(np.mean(np.asarray(inlier_cloud.points)[:, 2]))
-                plane_x_points.append(np.min(np.asarray(inlier_cloud.points)[:, 0]))
-                
-            elif np.abs(normal_vector[2]) < 0.05:
-                inlier_cloud = point_cloud.select_by_index(inliers)
-                labels = np.array(inlier_cloud.cluster_dbscan(eps=0.05, min_points=10, print_progress=False))
-                valid_labels = labels[labels != -1]
-
-                if valid_labels.size < 1:
-                    copy_point_cloud = copy_point_cloud.select_by_index(inliers, invert=True)
-                    continue
-                # Find the largest valid cluster of points
-                unique_labels, counts = np.unique(labels, return_counts=True)
-                largest_cluster_label = unique_labels[np.argmax(counts)]
-                largest_cluster_indices = np.where(labels == largest_cluster_label)[0]
-                inlier_cloud = inlier_cloud.select_by_index(largest_cluster_indices)
-                z_points = np.asarray(inlier_cloud.points)[:, 2]
-                vertical_z_points.append(np.max(z_points))
-                vertical_x_points.append(np.mean(np.asarray(inlier_cloud.points)[:, 0]))
-
-            # Remove inliers from the point cloud
-            copy_point_cloud = copy_point_cloud.select_by_index(inliers, invert=True)
-
-        height_diff = 0
-
-        if len(vertical_z_points) > 0:
-            closest_cloud_index = np.argmin(vertical_x_points)
-            plane_height = vertical_z_points[closest_cloud_index]
-            height_diff = plane_height + 0.4
-            closest_x_distance = vertical_x_points[closest_cloud_index]
-        elif len(plane_x_points) > 0:
-            closest_cloud_index = np.argmin(plane_x_points)
-            plane_height = plane_z_points[closest_cloud_index]
-            height_diff = plane_height + 0.4
-            closest_x_distance = plane_x_points[closest_cloud_index]
+    def resolve_closest_x_point(self, height_diff: float, obstacle_z_points, obstacle_x_points) -> float:
+        if obstacle_z_points.size <= 0 or obstacle_x_points.size <= 0: 
+            return 10.0
+        if 0.25 > height_diff > 0.1:
+            threshold = np.max(obstacle_z_points) - 0.05
+            hor_plane = obstacle_x_points[obstacle_z_points >= threshold]
+        elif -0.25 < height_diff < -0.04:
+            threshold = np.max(obstacle_z_points) + 0.05
+            hor_plane = obstacle_x_points[obstacle_z_points <= threshold]
         else:
-            closest_x_distance = 0
-        smoothened_height_diff = (sum(self.moving_average_height_diff) + height_diff) / (len(self.moving_average_height_diff) + 1)
-        self.add_height_to_storage(smoothened_height_diff, self.moving_average_height_diff)
+            return 10.0
+        closest_x_point = np.min(hor_plane)
+        return closest_x_point
+    
+    def resolve_step(self, height_diff: float | None, closest_x_distance: float):
         self.stair_msg.distance = float(closest_x_distance)
         self.stair_msg.detected = False
         self.stair_msg.upstairs = False
-        if 0.15 > smoothened_height_diff > 0.05:
-        #if 0.15 > smoothened_height_diff > 0.05:
-            #self.get_logger().info("step up!")
+        if not height_diff:
+            self.get_logger().info("no step!")
+        elif 0.25 > height_diff > 0.1:
+            self.get_logger().info("step up!")
             self.stair_msg.upstairs = True
             self.stair_msg.detected = True
-        elif -1 < smoothened_height_diff < -0.01:
-            #self.get_logger().info("step down!")
+        elif -0.25 < height_diff < -0.04:
+            self.get_logger().info("step down!")
             self.stair_msg.detected = True
+        else:
+            self.get_logger().info("no step!")
         
         self.publisher_.publish(self.stair_msg)
+    
+    def convert_open3d_to_ros(self, open3d_cloud, frame_id: str = "map", rgb: int = 255):
+        """Convert an Open3D PointCloud to a ROS2 PointCloud2 message."""
         
+        points = np.asarray(open3d_cloud.points, dtype=np.float32)
+        rgba = np.zeros((points.shape[0],), dtype=np.float32)
+        rgba[:] = rgb
 
-    def add_height_to_storage(self, height_diff: float, storage: deque) -> list[float]:
-        storage.append(height_diff)
-        if len(storage) > 3:
-            storage.popleft()
+        cloud_data = np.column_stack((points, rgba))
+
+        msg = PointCloud2()
+        msg.header = Header()
+        msg.header.frame_id = frame_id
+        msg.height = 1  # Unordered cloud (1D list of points)
+        msg.width = points.shape[0]
+        msg.is_dense = False
+        msg.is_bigendian = False
+        msg.point_step = 16  # Each point has (x,y,z,rgba), 4 * 4 bytes each
+        msg.row_step = msg.point_step * msg.width
+
+        msg.fields = [
+            PointField(name="x", offset=0, datatype=PointField.FLOAT32, count=1),
+            PointField(name="y", offset=4, datatype=PointField.FLOAT32, count=1),
+            PointField(name="z", offset=8, datatype=PointField.FLOAT32, count=1),
+            PointField(name="rgba", offset=12, datatype=PointField.FLOAT32, count=1),
+        ]
+        msg.data = cloud_data.tobytes()
+        
+        return msg
             
 def main(args=None):
     rclpy.init(args=args)
